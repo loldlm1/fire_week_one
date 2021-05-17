cf2221145d5d3dd1d9befcff8adb08d3fe538870
joccreng <joc.rengifoo@gmail.com>
2021-05-17 19:01:35 -0400

    Tarea del dia lunes creada en una clase

--- week.rb
+++ week.rb
@@ -1,2 +1,23 @@
-# Este es el archivo principal donde van a crear su clase con todos los metodos de la semana.
+romano = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"]
 
+
+class Week_Fire
+	def initialize(romano)
+		@romano = romano
+	end
+
+	def romano_a_numero(string)
+  		numero = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+  		@romano 
+  		hash = @romano.zip(numero).inject({}) {|r,ele| r[ele.first] = ele.last; r}
+  		reg = /M|CM|D|CD|C|XC|L|XL|X|IX|V|IV|I/
+  		temp = string.scan(reg)
+  		temp.uniq.inject(0) do |r,ele|
+    	r += temp.count(ele)*hash[ele]
+    	end
+  	end
+end
+
+week_fire = Week_Fire.new(romano)
+
+puts week_fire.romano_a_numero("VI")
