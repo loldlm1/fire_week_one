9aefe0bee198c4d0ca8355339ef221fb0e4b1df4
joccreng <joc.rengifoo@gmail.com>
2021-05-18 17:50:52 -0400

    eliminado el puts del metodo

--- week.rb
+++ week.rb
@@ -12,11 +12,11 @@ class Week
   temp = string.scan(reg)
   temp.uniq.inject(0) do |r,ele|
     r += temp.count(ele)*hash[ele]
-    puts "#{r}"
+    "#{r}"
   end
  end
 end
 
 week_fire = Week.new(romano)
 
-week_fire.romano_a_numero("XL")
+puts week_fire.romano_a_numero("XL")
