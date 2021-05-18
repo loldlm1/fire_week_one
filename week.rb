26e010c32ea8c93823cbe8b8e3f7bec9681942d4
joccreng <joc.rengifoo@gmail.com>
2021-05-18 17:03:30 -0400

    simulacion de likes, corregir

--- week.rb
+++ week.rb
@@ -1,9 +1,10 @@
 romano = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"]
-
+likes = to_s
 
 class Week
-	def initialize(romano)
+	def initialize(romano, likes)
 		@romano = romano
+    @likes = likes
 	end
 
 	def romano_a_numero(string)
@@ -16,8 +17,28 @@ class Week
   			puts "#{r}"
       end
   	end
+
+    def facebook(likes)
+      case likes
+      when 0..999
+        puts "#{likes}k"
+      when 1.000..9.000
+        puts "#{likes}K"
+      when 10.000..90.000
+        puts "#{likes}K" 
+      when 100.000..999.999
+        puts "#{likes}K"
+      when 1_000_000..999_000_000
+        puts "#{likes.to_i}M"
+      end
+    end
 end
 
-week_fire = Week.new(romano)
+week_fire = Week.new(romano, likes)
 
 week_fire.romano_a_numero("XL")
+week_fire.facebook(422)
+week_fire.facebook(9.000.to_i)
+week_fire.facebook(20.000.to_i)
+week_fire.facebook(340.000.to_i)
+week_fire.facebook(5_000_00.to_i)
