0179c050a5a6d58cf871104cfbee2c6b2a66f02e
joccreng <joc.rengifoo@gmail.com>
2021-05-18 17:57:43 -0400

    identacion arreglada

--- week.rb
+++ week.rb
@@ -1,20 +1,20 @@
 romano = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"]
 
 class Week
- def initialize(romano)
-  @romano = romano
- end
-
- def romano_a_numero(string)
-  numero = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
-  hash = @romano.zip(numero).inject({}) {|r,ele| r[ele.first] = ele.last; r}
-  reg = /M|CM|D|CD|C|XC|L|XL|X|IX|V|IV|I/
-  temp = string.scan(reg)
-  temp.uniq.inject(0) do |r,ele|
-    r += temp.count(ele)*hash[ele]
-    "#{r}"
+  def initialize(romano)
+   @romano = romano
   end
- end
+
+  def romano_a_numero(string)
+   numero = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+   hash = @romano.zip(numero).inject({}) {|r,ele| r[ele.first] = ele.last; r}
+   reg = /M|CM|D|CD|C|XC|L|XL|X|IX|V|IV|I/
+   temp = string.scan(reg)
+   temp.uniq.inject(0) do |r,ele|
+     r += temp.count(ele)*hash[ele]
+     "#{r}"
+   end
+  end
 end
 
 week_fire = Week.new(romano)
